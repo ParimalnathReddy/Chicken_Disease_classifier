de2e0e6b0c9e435e69b16bec4b14438d39178a3a
ParimalnathReddy <Parimal@1234>
2026-02-01 01:51:29 -0500

    Add .gitignore, main.py and update files

---
+++ main.py
@@ -0,0 +1,3 @@
+from src.Chicken_Disease_classifier import logger
+
+logger.info("Welcome to the chicken disease classifier")
--- template.py
+++ template.py
@@ -1,43 +1,35 @@
-import os
-from pathlib import Path
 import logging
+from pathlib import Path
 
-logging.basicConfig(level=logging.INFO, format= '[%(asctime)s] %(levelname)s - %(message)s')
+logging.basicConfig(level=logging.INFO, format='[%(asctime)s] %(levelname)s - %(message)s')
 
-project_name = "Chicken_Disease_classifier"
+project_name = "chicken_disease_classifier"  # keep package names lowercase
 
 list_of_files = [
-    ".github/workflows/.gitkeep", # keep this file to avoid github errors
-    f"src/{project_name}/__init__.py",
-    f"src/{project_name}/components/__init__.py",
-    f"src/{project_name}/utils/__init__.py",
-    f"src/{project_name}/config/__init__.py",
-    f"src/{project_name}/config/configuration.py",
-    f"src/{project_name}/pipeline/__init__.py",
-    f"src/{project_name}/entity/__init__.py",
-    f"src/{project_name}/constants/__init__.py",   
-    "config/config.yaml", 
-    "setup.py",
-    "dvc.yaml",
-    "params.yaml",
-    "requirements.txt",
-    "research/trials.ipynb",
-    "README.md"
-
+    Path(".github/workflows/.gitkeep"),
+    Path(f"src/{project_name}/__init__.py"),
+    Path(f"src/{project_name}/components/__init__.py"),
+    Path(f"src/{project_name}/utils/__init__.py"),
+    Path(f"src/{project_name}/config/__init__.py"),
+    Path(f"src/{project_name}/config/configuration.py"),
+    Path(f"src/{project_name}/pipeline/__init__.py"),
+    Path(f"src/{project_name}/entity/__init__.py"),
+    Path(f"src/{project_name}/constants/__init__.py"),
+    Path("config/config.yaml"),
+    Path("setup.py"),
+    Path("dvc.yaml"),
+    Path("params.yaml"),
+    Path("requirements.txt"),
+    Path("research/trials.ipynb"),
+    Path("README.md"),
+    
 ]
 
-
 for file_path in list_of_files:
-    file_path = Path(file_path)
-    file_dir, file_name = os.path.split(file_path)
-    if file_dir != "":
-        os.makedirs(file_dir, exist_ok=True)
-        logging.info(f"Creating directory: {file_dir} for file: {file_name}")
-     
-    if (not file_path.exists()) or (os.path.getsize(file_path) == 0):
-        with open(file_path, 'w') as f:
-            pass
-        logging.info(f"Creating empty file: {file_path}")
-    
+    file_path.parent.mkdir(parents=True, exist_ok=True)
+
+    if not file_path.exists():
+        file_path.touch()
+        logging.info(f"Created file: {file_path}")
     else:
-        logging.info(f"File already exists: {file_path}")
+        logging.info(f"Already exists: {file_path}")
